fa32e7e6a3e9830f2b6d8fe0a42a474ae8012b2d
Valery Zvonarev <furysound1@yandex.ru>
2024-06-13 17:30:55 +0300

    changes after Sprint 04 review

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -21,7 +21,7 @@ final class MovieQuizViewController: UIViewController {
     }
     
     // вью-модель для состояния "Результат квиза"
-    struct QuizResultsViewModel {
+    private struct QuizResultsViewModel {
         let title: String                   // строка с заголовком алерта
         let text: String                     // строка с текстом о количестве набранных очков
         let buttonText: String   // текст для кнопки алерта
